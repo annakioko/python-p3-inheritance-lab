5f5499f6236162b34881af526d1185413b9b2de6
Anna Kioko <annakioko00@gmail.com>
2024-03-01 07:30:41 +0300

    completed assignment

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name, knowledge=('')):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.knowledge = knowledge
+
+        
